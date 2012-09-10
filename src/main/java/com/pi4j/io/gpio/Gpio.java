9041992e642db0c5e7fb7125a1c5f35e1aefe81c
robert@savagehomeautomation.com <robert@savagehomeautomation.com>
2012-09-10 08:58:29 -0500

    updated site docs

--- src/main/java/com/pi4j/io/gpio/Gpio.java
+++ src/main/java/com/pi4j/io/gpio/Gpio.java
@@ -1,3 +1,19 @@
+/*
+ * **********************************************************************
+ * This file is part of the pi4j project: http://www.pi4j.com/
+ * 
+ * pi4j is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * pi4j is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with pi4j. If not,
+ * see <http://www.gnu.org/licenses/>.
+ * **********************************************************************
+ */
 package com.pi4j.io.gpio;
 
 import java.util.List;
