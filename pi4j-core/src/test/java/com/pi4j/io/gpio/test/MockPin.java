26c509cb4e873f53ca1646dfdece2ea39c872b03
robert@savagehomeautomation.com <robert@savagehomeautomation.com>
2012-10-27 01:00:04 -0500

    added additional mock pins for tests

--- pi4j-core/src/test/java/com/pi4j/io/gpio/test/MockPin.java
+++ pi4j-core/src/test/java/com/pi4j/io/gpio/test/MockPin.java
@@ -1,5 +1,32 @@
 package com.pi4j.io.gpio.test;
 
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Java Library (Core)
+ * FILENAME      :  MockPin.java  
+ * 
+ * This file is part of the Pi4J project. More information about 
+ * this project can be found here:  http://www.pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 Pi4J
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.util.EnumSet;
 
 import com.pi4j.io.gpio.Pin;
@@ -20,12 +47,15 @@ public class MockPin
     public static final Pin DIGITAL_OUTPUT_PIN = new PinImpl(MockGpioProvider.NAME, 2, "GPIO-2", 
                                                             EnumSet.of(PinMode.DIGITAL_OUTPUT));
     
-    public static final Pin GPIO_01 = createDigitalAndPwmPin(1, "GPIO 1"); // PIN 1 supports PWM output
-    
-    private static Pin createDigitalAndPwmPin(int address, String name)
-    {
-        return new PinImpl(MockGpioProvider.NAME, address, name, 
-                           EnumSet.of(PinMode.DIGITAL_INPUT, PinMode.DIGITAL_OUTPUT, PinMode.PWM_OUTPUT),
-                           PinPullResistance.all());
-    }
+    public static final Pin PWM_OUTPUT_PIN = new PinImpl(MockGpioProvider.NAME, 3, "GPIO-3", 
+                                                         EnumSet.of(PinMode.PWM_OUTPUT));
+
+    public static final Pin ANALOG_BIDIRECTIONAL_PIN  = new PinImpl(MockGpioProvider.NAME, 4, "GPIO-4", 
+                                                                    EnumSet.of(PinMode.ANALOG_INPUT, 
+                                                                               PinMode.ANALOG_OUTPUT));
+    public static final Pin ANALOG_INPUT_PIN = new PinImpl(MockGpioProvider.NAME, 5, "GPIO-5", 
+                                                          EnumSet.of(PinMode.ANALOG_INPUT));
+
+    public static final Pin ANALOG_OUTPUT_PIN = new PinImpl(MockGpioProvider.NAME, 6, "GPIO-6", 
+                                                            EnumSet.of(PinMode.ANALOG_OUTPUT));
 }
