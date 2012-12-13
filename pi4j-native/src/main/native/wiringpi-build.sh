412fa12fd15105477a988fbcfd4b9a04c8ab6b96
robert@savagehomeautomation.com <robert@savagehomeautomation.com>
2012-12-13 01:31:21 -0600

    FIX: https://github.com/Pi4J/pi4j/issues/5 Modified build script to include pulling wiringPi sources and compiling when building native library

---
+++ pi4j-native/src/main/native/wiringpi-build.sh
@@ -0,0 +1,51 @@
+###
+# #%L
+# **********************************************************************
+# ORGANIZATION  :  Pi4J
+# PROJECT       :  Pi4J :: JNI Native Library
+# FILENAME      :  wiringpi-build.sh  
+# 
+# This file is part of the Pi4J project. More information about 
+# this project can be found here:  http://www.pi4j.com/
+# **********************************************************************
+# %%
+# Copyright (C) 2012 Pi4J
+# %%
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# #L%
+###
+
+# ----------------------------------
+# install prerequisites
+# ----------------------------------
+sudo apt-get install gcc -y
+sudo apt-get install git-core -y
+
+# ----------------------------------
+# clone wiringPi from github
+# ----------------------------------
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+git pull origin
+
+# ----------------------------------
+# uninstall any previous copies
+# ----------------------------------
+cd wiringPi
+sudo make uninstall
+
+# ----------------------------------
+# build latest wiringPi
+# ----------------------------------
+cd ..
+./build
