a0895864223f4fd5036e359a84315e3b7f88ce81
robert@savagehomeautomation.com <robert@savagehomeautomation.com>
2013-02-23 19:58:20 -0600

    added additional system information API methods to access memory, frequency, CPU temperature, voltage, and board model data. implements feature request defined in issue #30

--- pi4j-core/src/main/java/com/pi4j/system/SystemInfo.java
+++ pi4j-core/src/main/java/com/pi4j/system/SystemInfo.java
@@ -31,7 +31,10 @@ package com.pi4j.system;
 import java.io.IOException;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.pi4j.util.ExecUtil;
@@ -43,7 +46,14 @@ public class SystemInfo {
     private SystemInfo() {
         // forbid object construction 
     }
-        
+    
+    public enum BoardType {
+        UNKNOWN,
+        ModelA_Rev0,
+        ModelB_Rev1,
+        ModelB_Rev2
+    }    
+    
     private static Map<String, String> cpuInfo;
 
     private static String getCpuInfo(String target) throws IOException, InterruptedException {
@@ -124,6 +134,28 @@ public class SystemInfo {
         return System.getProperty("os.arch");
     }
     
+    public static String getOsFirmwareBuild() throws IOException, InterruptedException {
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd version");
+        if(result != null){
+            for(String line : result) {
+                if(line.startsWith("version ")){                
+                    return line.substring(8);
+                }
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }    
+
+    public static String getOsFirmwareDate() throws IOException, InterruptedException, ParseException {
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd version");
+        if(result != null){
+            for(String line : result) {
+                return line; // return 1st line
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }    
+    
     public static String getJavaVendor()  {
         return System.getProperty("java.vendor");
     }
@@ -168,6 +200,236 @@ public class SystemInfo {
                 return false;
             } } );
     }
+    
+
+    private static List<Long> getMemory() throws IOException, InterruptedException {
+        // Memory information is in the form
+        // root@mypi:/home/pi# free -b
+        //              total       used       free     shared    buffers     cached
+        // Mem:     459771904  144654336  315117568          0   21319680   63713280
+        // -/+ buffers/cache:   59621376  400150528
+        // Swap:    104853504          0  104853504
+        List<Long> values = new ArrayList<>();
+        String result[] = ExecUtil.execute("free -b");
+        if(result != null){
+            for(String line : result) {
+                if(line.startsWith("Mem:")){
+                    String parts[] = line.split(" ");                    
+                    for(String part : parts){
+                        part = part.trim();
+                        if(!part.isEmpty() && !part.equalsIgnoreCase("Mem:")){
+                            values.add(new Long(part));
+                        }
+                    }
+                }
+            }
+        }
+        return values;
+    }
+    
+    public static long getMemoryTotal() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 0){
+            return values.get(0); // total memory value is in first position
+        }
+        return -1;
+    }
+
+    public static long getMemoryUsed() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 1){
+            return values.get(1); // used memory value is in second position
+        }
+        return -1;
+    }
+
+    public static long getMemoryFree() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 2){
+            return values.get(2); // free memory value is in third position
+        }
+        return -1;
+    }
+
+    public static long getMemoryShared() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 3){
+            return values.get(3); // shared memory value is in fourth position
+        }
+        return -1;
+    }
+
+    public static long getMemoryBuffers() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 4){
+            return values.get(4); // buffers memory value is in fifth position
+        }
+        return -1;
+    }
+
+    public static long getMemoryCached() throws IOException, InterruptedException {
+        List<Long> values = getMemory();
+        if(!values.isEmpty() && values.size() > 5){
+            return values.get(5); // cached memory value is in sixth position
+        }
+        return -1;
+    }
+
+    public static BoardType getBoardType() throws IOException, InterruptedException
+    {
+        // The following info obtained from:
+        // http://www.raspberrypi.org/archives/1929
+        // http://raspberryalphaomega.org.uk/?p=428
+        // http://www.raspberrypi.org/phpBB3/viewtopic.php?p=281039#p281039
+        switch(getRevision())
+        {
+        case "0002":  // Model B Revision 1
+        case "0003":  // Model B Revision 1 + Fuses mod and D14 removed
+            return BoardType.ModelB_Rev1;
+        case "0004":  // Model B Revision 2 256MB (Sony)
+        case "0005":  // Model B Revision 2 256MB (Qisda)
+        case "0006":  // Model B Revision 2 256MB (Egoman)
+            return BoardType.ModelB_Rev2;
+        case "0007":  // Model A 256MB (Egoman)
+        case "0008":  // Model A 256MB (Sony)
+        case "0009":  // Model A 256MB (Qisda)
+            return BoardType.ModelA_Rev0;
+        case "000d":  // Model B Revision 2 512MB (Egoman)
+        case "000e":  // Model B Revision 2 512MB (Sony)
+        case "000f":  // Model B Revision 2 512MB (Qisda)
+            return BoardType.ModelB_Rev2;
+        default:
+            return BoardType.UNKNOWN;
+        }
+    }
+
+    public static float getCpuTemperature() throws IOException, InterruptedException, NumberFormatException {
+        // CPU temperature is in the form
+        // pi@mypi$ /opt/vc/bin/vcgencmd measure_temp
+        // temp=42.3'C
+        // Support for this was added around firmware version 3357xx per info
+        // at http://www.raspberrypi.org/phpBB3/viewtopic.php?p=169909#p169909
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd measure_temp");
+        if(result != null){
+            for(String line : result) {
+                String parts[] = line.split("[=']", 3);                
+                return Float.parseFloat(parts[1]);
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }
+
+    private static float getVoltage(String id) throws IOException, InterruptedException, NumberFormatException {
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd measure_volts " + id);
+        if(result != null){
+            for(String line : result) {
+                String parts[] = line.split("[=V]", 3);                
+                return Float.parseFloat(parts[1]);
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }
+    
+    public static float getCpuVoltage() throws IOException, InterruptedException, NumberFormatException {
+        return getVoltage("core");
+    }
+
+    public static float getMemoryVoltageSDRam_C() throws IOException, InterruptedException, NumberFormatException {
+        return getVoltage("sdram_c");
+    }
+
+    public static float getMemoryVoltageSDRam_I() throws IOException, InterruptedException, NumberFormatException {
+        return getVoltage("sdram_i");
+    }
+
+    public static float getMemoryVoltageSDRam_P() throws IOException, InterruptedException, NumberFormatException {
+        return getVoltage("sdram_p");
+    }
+    
+
+    private static boolean getCodecEnabled(String codec) throws IOException, InterruptedException {
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd codec_enabled " + codec);
+        if(result != null){
+            for(String line : result) {
+                String parts[] = line.split("=", 2);                
+                return parts[1].trim().equalsIgnoreCase("enabled");
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }
+    
+    public static boolean getCodecH264Enabled() throws IOException, InterruptedException {
+        return getCodecEnabled("H264");
+    }
+
+    public static boolean getCodecMPG2Enabled() throws IOException, InterruptedException {
+        return getCodecEnabled("MPG2");
+    }
+
+    public static boolean getCodecWVC1Enabled() throws IOException, InterruptedException {
+        return getCodecEnabled("WVC1");
+    }
+    
+
+    private static long getClockFrequency(String target) throws IOException, InterruptedException {
+        String result[] = ExecUtil.execute("/opt/vc/bin/vcgencmd measure_clock " + target.trim());
+        if(result != null){
+            for(String line : result) {
+                String parts[] = line.split("=", 2);                
+                return Long.parseLong(parts[1].trim());
+            }
+        }
+        throw new RuntimeException("Invalid command or response.");
+    }
+    
+    public static long getClockFrequencyArm() throws IOException, InterruptedException {
+        return getClockFrequency("arm");
+    }
+
+    public static long getClockFrequencyCore() throws IOException, InterruptedException {
+        return getClockFrequency("core");
+    }
+
+    public static long getClockFrequencyH264() throws IOException, InterruptedException {
+        return getClockFrequency("h264");
+    }
+
+    public static long getClockFrequencyISP() throws IOException, InterruptedException {
+        return getClockFrequency("isp");
+    }
+
+    public static long getClockFrequencyV3D() throws IOException, InterruptedException {
+        return getClockFrequency("v3d");
+    }
+
+    public static long getClockFrequencyUART() throws IOException, InterruptedException {
+        return getClockFrequency("uart");
+    }
+
+    public static long getClockFrequencyPWM() throws IOException, InterruptedException {
+        return getClockFrequency("pwm");
+    }
+
+    public static long getClockFrequencyEMMC() throws IOException, InterruptedException {
+        return getClockFrequency("emmc");
+    }
+    
+    public static long getClockFrequencyPixel() throws IOException, InterruptedException {
+        return getClockFrequency("pixel");
+    }
+    
+    public static long getClockFrequencyVEC() throws IOException, InterruptedException {
+        return getClockFrequency("vec");
+    }
+    
+    public static long getClockFrequencyHDMI() throws IOException, InterruptedException {
+        return getClockFrequency("hdmi");
+    }
+
+    public static long getClockFrequencyDPI() throws IOException, InterruptedException {
+        return getClockFrequency("dpi");
+    }
+    
     
     /*
      * this method will to obtain the version info string from the 'bash' program
--- pi4j-example/src/main/java/SystemInfoExample.java
+++ pi4j-example/src/main/java/SystemInfoExample.java
@@ -28,8 +28,8 @@
  * #L%
  */
 
-
 import java.io.IOException;
+import java.text.ParseException;
 
 import com.pi4j.system.NetworkInfo;
 import com.pi4j.system.SystemInfo;
@@ -42,7 +42,7 @@ import com.pi4j.system.SystemInfo;
  */
 public class SystemInfoExample {
     
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException, ParseException {
         
         // display a few of the available system information properties
         System.out.println("----------------------------------------------------");
@@ -52,10 +52,26 @@ public class SystemInfoExample {
         System.out.println("CPU Revision      :  " + SystemInfo.getCpuRevision());
         System.out.println("CPU Architecture  :  " + SystemInfo.getCpuArchitecture());
         System.out.println("CPU Part          :  " + SystemInfo.getCpuPart());
+        System.out.println("CPU Temperature   :  " + SystemInfo.getCpuTemperature());
+        System.out.println("CPU Core Voltage  :  " + SystemInfo.getCpuVoltage());
         System.out.println("MIPS              :  " + SystemInfo.getBogoMIPS());
         System.out.println("Processor         :  " + SystemInfo.getProcessor());
         System.out.println("Hardware Revision :  " + SystemInfo.getRevision());
         System.out.println("Is Hard Float ABI :  " + SystemInfo.isHardFloatAbi());
+        System.out.println("Board Type        :  " + SystemInfo.getBoardType().name());
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("MEMORY INFO");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Total Memory      :  " + SystemInfo.getMemoryTotal());
+        System.out.println("Used Memory       :  " + SystemInfo.getMemoryUsed());
+        System.out.println("Free Memory       :  " + SystemInfo.getMemoryFree());
+        System.out.println("Shared Memory     :  " + SystemInfo.getMemoryShared());
+        System.out.println("Memory Buffers    :  " + SystemInfo.getMemoryBuffers());
+        System.out.println("Cached Memory     :  " + SystemInfo.getMemoryCached());
+        System.out.println("SDRAM_C Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_C());
+        System.out.println("SDRAM_I Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_I());
+        System.out.println("SDRAM_P Voltage   :  " + SystemInfo.getMemoryVoltageSDRam_P());
 
         System.out.println("----------------------------------------------------");
         System.out.println("OPERATING SYSTEM INFO");
@@ -63,6 +79,8 @@ public class SystemInfoExample {
         System.out.println("OS Name           :  " + SystemInfo.getOsName());
         System.out.println("OS Version        :  " + SystemInfo.getOsVersion());
         System.out.println("OS Architecture   :  " + SystemInfo.getOsArch());
+        System.out.println("OS Firmware Build :  " + SystemInfo.getOsFirmwareBuild());
+        System.out.println("OS Firmware Date  :  " + SystemInfo.getOsFirmwareDate());
         
         System.out.println("----------------------------------------------------");
         System.out.println("JAVA ENVIRONMENT INFO");
@@ -85,6 +103,33 @@ public class SystemInfoExample {
             System.out.println("FQDN              :  " + fqdn);
         for (String nameserver : NetworkInfo.getNameservers())
             System.out.println("Nameserver        :  " + nameserver);
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("CODEC INFO");
+        System.out.println("----------------------------------------------------");
+        System.out.println("H264 Codec Enabled:  " + SystemInfo.getCodecH264Enabled());
+        System.out.println("MPG2 Codec Enabled:  " + SystemInfo.getCodecMPG2Enabled());
+        System.out.println("WVC1 Codec Enabled:  " + SystemInfo.getCodecWVC1Enabled());
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("CLOCK INFO");
+        System.out.println("----------------------------------------------------");
+        System.out.println("ARM Frequency     :  " + SystemInfo.getClockFrequencyArm());
+        System.out.println("CORE Frequency    :  " + SystemInfo.getClockFrequencyCore());
+        System.out.println("H264 Frequency    :  " + SystemInfo.getClockFrequencyH264());
+        System.out.println("ISP Frequency     :  " + SystemInfo.getClockFrequencyISP());
+        System.out.println("V3D Frequency     :  " + SystemInfo.getClockFrequencyV3D());
+        System.out.println("UART Frequency    :  " + SystemInfo.getClockFrequencyUART());
+        System.out.println("PWM Frequency     :  " + SystemInfo.getClockFrequencyPWM());
+        System.out.println("EMMC Frequency    :  " + SystemInfo.getClockFrequencyEMMC());
+        System.out.println("Pixel Frequency   :  " + SystemInfo.getClockFrequencyPixel());
+        System.out.println("VEC Frequency     :  " + SystemInfo.getClockFrequencyVEC());
+        System.out.println("HDMI Frequency    :  " + SystemInfo.getClockFrequencyHDMI());
+        System.out.println("DPI Frequency     :  " + SystemInfo.getClockFrequencyDPI());
+        
+            
+        System.out.println();
+        System.out.println();
     }
 }
 // END SNIPPET: system-info-snippet
