38f44da7d142f11d093802882f7594e60eda336b
robert@savagehomeautomation.com <robert@savagehomeautomation.com>
2013-03-17 23:33:10 -0500

    comment update to piface gpio example

--- pi4j-example/src/main/java/PiFaceGpioExample.java
+++ pi4j-example/src/main/java/PiFaceGpioExample.java
@@ -5,7 +5,7 @@
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Java Examples
- * FILENAME      :  MCP23017GpioExample.java  
+ * FILENAME      :  PiFaceGpioExample.java  
  * 
  * This file is part of the Pi4J project. More information about 
  * this project can be found here:  http://www.pi4j.com/
