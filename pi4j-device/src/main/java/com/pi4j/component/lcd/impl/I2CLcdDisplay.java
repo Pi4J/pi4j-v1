e37ee9a449bbacd72d5aac08a9fd32c98e7d7963
schandle <andy.chandler@williams.com>
2013-08-31 19:51:44 -0500

    Borrows HEAVILY from work done at http://pebrou.wordpress.com/     Use translate.google.com if you speak english.   This implementation should work the same as the GPIOLcd  implentation from an interface perspective.   I was unable to get the GPIO version to work with the PCF8574 gpio extension though and I suspect that has to do with the lower level native code as simple tests of that extension seemed to work with lcd's.

--- pi4j-device/src/main/java/com/pi4j/component/lcd/impl/I2CLcdDisplay.java
+++ pi4j-device/src/main/java/com/pi4j/component/lcd/impl/I2CLcdDisplay.java
@@ -1,11 +1,26 @@
+/*
+ * @(#)I2CLcdDisplay.java   08/29/13
+ * 
+ * Copyright (c) 2012 Williams Company
+ *
+ * License agreement text here ...
+ *
+ *
+ *
+ */
+
+
+
 package com.pi4j.component.lcd.impl;
 
+//~--- non-JDK imports --------------------------------------------------------
+
 /*
- * #%L
+* #%L
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Device Abstractions
- * FILENAME      :  GpioLcdDisplay.java  
+ * FILENAME      :  I2CLcdDisplay.java  
  * 
  * This file is part of the Pi4J project. More information about 
  * this project can be found here:  http://www.pi4j.com/
@@ -26,47 +41,154 @@ package com.pi4j.component.lcd.impl;
  * limitations under the License.
  * #L%
  */
-
-
 import com.pi4j.component.lcd.LCD;
 import com.pi4j.component.lcd.LCDBase;
 import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.wiringpi.Lcd;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class GpioLcdDisplay extends LCDBase implements LCD 
-{
-    protected int rows;
-    protected int columns;
-    private int lcdHandle;
-    
-    public GpioLcdDisplay(int rows, int columns, Pin rsPin, Pin strobePin, Pin... dataPins) {
-        this.rows = rows;
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Class description
+ *
+ *
+ * @version        1.0, 08/29/13
+ * @author         schandle
+ */
+public class I2CLcdDisplay extends LCDBase implements LCD {
+    boolean             backLightDesiredState = true;
+    boolean             backlight             = false;
+    boolean             rsFlag                = false;
+    boolean             eFlag                 = false;
+    private I2CDevice   dev                   = null;
+    private final int[] LCD_LINE_ADDRESS      = { 0x80, 0xC0, 0x94, 0xD4 };
+
+    /** posilame text */
+    private final boolean LCD_CHR = true;
+
+    /** posilame command */
+    private final boolean LCD_CMD = false;
+    protected int         rows;
+    protected int         columns;
+  //  private int           lcdHandle;
+    private int pulseData = 0;
+
+    /**
+     * Constructs ...
+     *
+     *
+     * @param rows
+     * @param columns
+     * @param i2cBus
+     * @param i2cAddress
+     * @param backlightBit
+     * @param rsBit
+     * @param rwBit
+     * @param eBit
+     * @param d7
+     * @param d6
+     * @param d5
+     * @param d4
+     *
+     * @throws Exception
+     */
+    public I2CLcdDisplay(int rows, int columns, int i2cBus, int i2cAddress, int backlightBit, int rsBit, int rwBit,
+                         int eBit, int d7, int d6, int d5, int d4)
+            throws Exception {
+        this.rows    = rows;
         this.columns = columns;
-        int bits[] = { 0,0,0,0,0,0,0,0 };
-        
-        // set wiringPi interface for internal use
-        // we will use the WiringPi pin number scheme with the wiringPi library
-        com.pi4j.wiringpi.Gpio.wiringPiSetup();        
-        
-        // seed bit pin address array
-        for(int index = 0; index < 8; index++) {
-            if(index < dataPins.length)
-                bits[index] = dataPins[index].getAddress();
-        }
-        
-        // initialize LCD
-        lcdHandle = Lcd.lcdInit(rows, 
-                                columns, 
-                                dataPins.length, 
-                                rsPin.getAddress(), 
-                                strobePin.getAddress(), 
-                                bits[0], bits[1], bits[2], bits[3], bits[4], bits[5], bits[6], bits[7]);
-        
-        // verify LCD initialization
-        if (lcdHandle == -1) 
-            throw new RuntimeException("Invalid LCD handle returned from wiringPi: " + lcdHandle);
+
+        int bits[] = { d7, d6, d5, d4 };
+
+        this.rows    = rows;
+        this.columns = columns;
+
+        I2CBus bus = I2CFactory.getInstance(i2cBus);
+
+        dev = bus.getDevice(i2cAddress);
+        this.init();
     }
-    
+
+    @Override
+    public void clear() {
+        try {
+            lcd_byte(0x01,LCD_CMD);
+        } catch (Exception ex) {
+            Logger.getLogger(I2CLcdDisplay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void write(byte data) {
+        try {
+            lcd_byte(data,LCD_CHR);
+        } catch (Exception ex) {
+            Logger.getLogger(I2CLcdDisplay.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void write(String data) {
+        for(int i = 0; i < data.length(); i++){
+            try{
+                lcd_byte(data.charAt(i),LCD_CHR);
+            }catch(Exception e){
+                System.out.println("Problems writing data");
+            }
+    }
+    }
+
+    private void init() throws Exception {
+        lcd_byte(0x33, LCD_CMD);    // 4 bit
+        lcd_byte(0x32, LCD_CMD);    // 4 bit
+        lcd_byte(0x28, LCD_CMD);    // 4bit - 2 line
+        lcd_byte(0x08, LCD_CMD);    // don't shift, hide cursor
+        lcd_byte(0x01, LCD_CMD);    // clear and home display
+        lcd_byte(0x06, LCD_CMD);    // move cursor right
+        lcd_byte(0x0c, LCD_CMD);    // turn on
+    }
+
+    public void lcd_byte(int val, boolean type) throws Exception {
+
+        // typ zapisu
+        setRS(type);
+
+        // High Bit
+        write(val >> 4);
+        pulse_en(type,val >>4 );    // cmd or display data
+
+        // lowbit
+        write(val & 0x0f);
+        pulse_en(type,val & 0x0f);
+    }
+
+    private void pulse_en(boolean type, int val) throws Exception {
+        setE(true);
+        write(val);
+        setE(false);
+        write(val);
+
+        // po CMD by se melo chvilku pockat
+        if (type == LCD_CMD) {
+            Thread.sleep(1);
+        }
+    }    // private voi
+
+    private void write(int tmpData) throws Exception {
+        byte out = 
+                (byte) (tmpData 
+                | (backlight ? 128 : 0) 
+                | (rsFlag ? 64 : 0) 
+                | (eFlag  ? 16 : 0));
+
+        dev.write(out);
+    }
+
     @Override
     public int getRowCount() {
         return rows;
@@ -78,28 +200,30 @@ public class GpioLcdDisplay extends LCDBase implements LCD
     }
 
     @Override
-    public void clear() {
-        Lcd.lcdClear(lcdHandle);
-    }
-    
-    @Override
     public void setCursorHome() {
-        Lcd.lcdHome(lcdHandle);
-    }
-    
-    @Override
-    public void setCursorPosition(int row, int column) {
-        validateCoordinates(row, column);
-        Lcd.lcdPosition(lcdHandle, column, row);
+        try {
+            lcd_byte(0x02,LCD_CMD);
+        } catch (Exception ex) {
+            Logger.getLogger(I2CLcdDisplay.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void write(byte data) {
-        Lcd.lcdPutchar(lcdHandle, data);
+    public void setCursorPosition(int row, int column) {
+        validateCoordinates(row, column);
+        try {
+            lcd_byte(LCD_LINE_ADDRESS[row] + column,LCD_CMD);
+            // Lcd.lcdPosition(lcdHandle, column, row);
+        } catch (Exception ex) {
+            Logger.getLogger(I2CLcdDisplay.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    @Override
-    public void write(String data) {
-        Lcd.lcdPuts(lcdHandle, data);
-    }    
+
+    private void setRS(boolean val) {
+        rsFlag = val;
+    }
+
+    private void setE(boolean val) {
+        eFlag = val;
+    }
 }
