3822d6792a09acab28639ec85bb4c74bff547e29
Andrew Chandler <andy.chandler@williams.com>
2014-02-06 09:03:59 -0600

    IDE auto-updated all copyright dates to include 2014

--- pi4j-device/src/main/java/com/pi4j/component/temperature/impl/Tmp102.java
+++ pi4j-device/src/main/java/com/pi4j/component/temperature/impl/Tmp102.java
@@ -21,7 +21,7 @@ package com.pi4j.component.temperature.impl;
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
- * Copyright (C) 2012 - 2013 Pi4J
+ * Copyright (C) 2012 - 2014 Pi4J
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- pi4j-gpio-extension/src/main/java/com/pi4j/gpio/extension/ads/ADS1115Pin.java
+++ pi4j-gpio-extension/src/main/java/com/pi4j/gpio/extension/ads/ADS1115Pin.java
@@ -11,7 +11,7 @@ package com.pi4j.gpio.extension.ads;
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
- * Copyright (C) 2012 - 2013 Pi4J
+ * Copyright (C) 2012 - 2014 Pi4J
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
