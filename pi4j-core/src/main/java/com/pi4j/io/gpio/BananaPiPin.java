34f03f659f50b9bf9135e661cc750a01880beb4c
Robert Savage <robert@shadeblue.com>
2015-11-15 20:16:59 -0500

    (experimental feature) adding support for a BananaPi GPIO provider. (experimental feature) adding support for a BananaPi GPIO pins.

---
+++ pi4j-core/src/main/java/com/pi4j/io/gpio/BananaPiPin.java
@@ -0,0 +1,70 @@
+package com.pi4j.io.gpio;
+
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Java Library (Core)
+ * FILENAME      :  BananaPiPin.java  
+ * 
+ * This file is part of the Pi4J project. More information about 
+ * this project can be found here:  http://www.pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2015 Pi4J
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+
+/**
+ * BananaPi pin definitions for (default) WiringPi pin numbering scheme.
+ *
+ * @author Robert Savage (<a
+ *         href="http://www.savagehomeautomation.com">http://www.savagehomeautomation.com</a>)
+ */
+public class BananaPiPin extends PinBase {
+
+    public static final Pin GPIO_00 = createDigitalPin(0, "GPIO 0");
+    public static final Pin GPIO_01 = createDigitalAndPwmPin(1, "GPIO 1"); // supports PWM
+    public static final Pin GPIO_02 = createDigitalPin(2, "GPIO 2");
+    public static final Pin GPIO_03 = createDigitalPin(3, "GPIO 3");
+    public static final Pin GPIO_04 = createDigitalPin(4, "GPIO 4");
+    public static final Pin GPIO_05 = createDigitalPin(5, "GPIO 5");
+    public static final Pin GPIO_06 = createDigitalPin(6, "GPIO 6");
+    public static final Pin GPIO_07 = createDigitalPin(7, "GPIO 7");
+    public static final Pin GPIO_08 = createDigitalPin(8, "GPIO 8");
+    public static final Pin GPIO_09 = createDigitalPin(9, "GPIO 9");
+    public static final Pin GPIO_10 = createDigitalPin(10, "GPIO 10");
+    public static final Pin GPIO_11 = createDigitalPin(11, "GPIO 11");
+    public static final Pin GPIO_12 = createDigitalPin(12, "GPIO 12");
+    public static final Pin GPIO_13 = createDigitalPin(13, "GPIO 13");
+    public static final Pin GPIO_14 = createDigitalPin(14, "GPIO 14");
+    public static final Pin GPIO_15 = createDigitalPin(15, "GPIO 15");
+    public static final Pin GPIO_16 = createDigitalPin(16, "GPIO 16");
+    public static final Pin GPIO_17 = createDigitalPin(17, "GPIO 17");
+    public static final Pin GPIO_18 = createDigitalPin(18, "GPIO 18");
+    public static final Pin GPIO_19 = createDigitalPin(19, "GPIO 19");
+    public static final Pin GPIO_20 = createDigitalPin(20, "GPIO 20");
+
+    protected static Pin createDigitalPin(int address, String name) {
+        return createDigitalPin(BananaPiGpioProvider.NAME, address, name);
+    }
+
+    protected static Pin createDigitalAndPwmPin(int address, String name) {
+        return createDigitalAndPwmPin(BananaPiGpioProvider.NAME, address, name);
+    }
+}
---
+++ pi4j-example/src/main/java/com/pi4j/io/gpio/BananaPiListenGpioExample.java
@@ -0,0 +1,89 @@
+package com.pi4j.io.gpio;/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Java Examples
+ * FILENAME      :  BananaProListenGpioExample.java  
+ * 
+ * This file is part of the Pi4J project. More information about 
+ * this project can be found here:  http://www.pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2015 Pi4J
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+/**
+ * This example code demonstrates how to setup a listener
+ * for GPIO pin state changes on the BananaPi.
+ *
+ * The internal resistance is set to PULL UP. So when
+ * connecting GPIO pin #2 to a ground pin, you should
+ * see the GpioPinListenerDigital fire the event.
+ *
+ * @author Robert Savage
+ */
+public class BananaPiListenGpioExample {
+
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
+
+        // ####################################################################
+        //
+        // since we are not using the default Raspberry Pi platform, we should
+        // assign the default provider as the BananaPi provider.
+        //
+        // ####################################################################
+        GpioFactory.setDefaultProvider(new BananaPiGpioProvider());
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // ####################################################################
+        //
+        // When provisioning a pin, use the BananaPiPin class
+        //
+        // ####################################################################
+
+        // provision gpio pin #02 as an input pin with its internal pull up resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(BananaPiPin.GPIO_02, PinPullResistance.PULL_UP);
+
+        // create and register gpio pin listener
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+
+        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+
+        // keep program running until user aborts (CTRL-C)
+        while(true) {
+            Thread.sleep(500);
+        }
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
+    }
+}
