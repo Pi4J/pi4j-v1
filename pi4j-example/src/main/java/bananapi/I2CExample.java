e1b512642353457e9ed7ecfb1df682da8a9aea86
natdan <daniel.sendula@creative-sphere.com>
2016-01-02 21:01:11 +0000

    Fixed pi4-example

--- pi4j-example/src/main/java/bananapi/I2CExample.java
+++ pi4j-example/src/main/java/bananapi/I2CExample.java
@@ -4,9 +4,9 @@ package bananapi;
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Java Examples
- * FILENAME      :  I2CExample.java  
- * 
- * This file is part of the Pi4J project. More information about 
+ * FILENAME      :  I2CExample.java
+ *
+ * This file is part of the Pi4J project. More information about
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
@@ -16,29 +16,27 @@ package bananapi;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * #L%
  */
 
-import com.pi4j.io.gpio.*;
+import java.io.IOException;
+
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
-import com.pi4j.io.i2c.impl.I2CBusImplBananaPi;
+import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import com.pi4j.platform.Platform;
 import com.pi4j.platform.PlatformAlreadyAssignedException;
 import com.pi4j.platform.PlatformManager;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 /**
  * This example code demonstrates how to perform simple I2C
@@ -88,8 +86,9 @@ public class I2CExample {
      * @throws InterruptedException
      * @throws PlatformAlreadyAssignedException
      * @throws IOException
+     * @throws UnsupportedBusNumberException
      */
-    public static void main(String[] args) throws InterruptedException, PlatformAlreadyAssignedException, IOException {
+    public static void main(String[] args) throws InterruptedException, PlatformAlreadyAssignedException, IOException, UnsupportedBusNumberException {
 
         // ####################################################################
         //
