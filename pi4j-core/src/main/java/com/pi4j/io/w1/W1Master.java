0dddc9cf6fb9c5d4bcdb7cb4d8ad8b6bffb91298
natdan <daniel.sendula@creative-sphere.com>
2016-01-04 17:29:33 +0000

    Exposed deviceTypeMap in W1Master

--- pi4j-core/src/main/java/com/pi4j/io/w1/W1Master.java
+++ pi4j-core/src/main/java/com/pi4j/io/w1/W1Master.java
@@ -19,9 +19,9 @@ import java.util.logging.Logger;
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Java Library (Core)
- * FILENAME      :  W1Master.java  
- * 
- * This file is part of the Pi4J project. More information about 
+ * FILENAME      :  W1Master.java
+ *
+ * This file is part of the Pi4J project. More information about
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
@@ -31,12 +31,12 @@ import java.util.logging.Logger;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -147,6 +147,10 @@ public class W1Master {
         return deviceTypes;
     }
 
+    public Map<String, W1DeviceType> getDeviceTypeMap() {
+        return deviceTypeMap;
+    }
+
     private List<File> getDeviceDirs() {
         final File[] slaveDevices = masterDir.listFiles(new FilenameFilter() {
             @Override
@@ -213,9 +217,16 @@ public class W1Master {
     }
 
     /*
-     * public <T extends W1Device> List<T> getDevices(final String deviceFamilyId) { List<W1Device> devices = new ArrayList<>(); for (W1Device device : readDevices()) {
-     *
-     * if (deviceFamilyId == null || deviceFamilyId.toUpperCase().equals(device.getId())) { devices.add(device); } } return (List<T>) devices; }
+    public <T extends W1Device> List<T> getDevices(final String deviceFamilyId) {
+        List<W1Device> devices = new ArrayList<>();
+        for (W1Device device : readDevices()) {
+
+            if (deviceFamilyId == null || deviceFamilyId.toUpperCase().equals(device.getId())) {
+                devices.add(device);
+            }
+        }
+        return (List<T>) devices;
+    }
      */
 
     /**
