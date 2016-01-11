51ec56961f702bc906d74378bf3335674858f3b0
Heikki Vesalainen <heikkivesalainen@yahoo.com>
2016-01-11 19:37:27 +0200

    GpioInterrupt modified to use a java thread for polling. Fixes #207

--- pi4j-native/src/main/native/com_pi4j_wiringpi_GpioInterrupt.h
+++ pi4j-native/src/main/native/com_pi4j_wiringpi_GpioInterrupt.h
@@ -9,7 +9,7 @@
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
- * Copyright (C) 2012 - 2015 Pi4J
+ * Copyright (C) 2012 - 2016 Pi4J
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -38,20 +38,29 @@ extern "C" {
 
 /*
  * Class:     com_pi4j_wiringpi_GpioInterrupt
- * Method:    enablePinStateChangeCallback
+ * Method:    initPoll
  * Signature: (I)I
  */
-JNIEXPORT jint JNICALL Java_com_pi4j_wiringpi_GpioInterrupt_enablePinStateChangeCallback
-  (JNIEnv *, jclass, jint);
+JNIEXPORT jint JNICALL Java_com_pi4j_wiringpi_GpioInterrupt_initPoll
+(JNIEnv *, jclass, jint);
 
 /*
  * Class:     com_pi4j_wiringpi_GpioInterrupt
- * Method:    disablePinStateChangeCallback
+ * Method:    pollPinStateChange
+ * Signature: (II)I
+ */
+JNIEXPORT jint JNICALL Java_com_pi4j_wiringpi_GpioInterrupt_pollPinStateChange
+  (JNIEnv *, jclass, jint, jint, jint);
+
+/*
+ * Class:     com_pi4j_wiringpi_GpioInterrupt
+ * Method:    closePoll
  * Signature: (I)I
  */
-JNIEXPORT jint JNICALL Java_com_pi4j_wiringpi_GpioInterrupt_disablePinStateChangeCallback
-  (JNIEnv *, jclass, jint);
+JNIEXPORT jint JNICALL Java_com_pi4j_wiringpi_GpioInterrupt_closePoll
+(JNIEnv *, jclass, jint);
 
+  
 #ifdef __cplusplus
 }
 #endif
