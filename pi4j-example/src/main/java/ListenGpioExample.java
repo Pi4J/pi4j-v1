0e0f71079344071dee4e2c20a12316c64bc29389
Robert Savage <robert@pi4j.com>
2016-04-11 14:43:05 -0400

    Merge branch 'master' of git://github.com/Brainsucker92/pi4j into Brainsucker92-master

--- pi4j-example/src/main/java/ListenGpioExample.java
+++ pi4j-example/src/main/java/ListenGpioExample.java
@@ -6,9 +6,9 @@
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Java Examples
- * FILENAME      :  ListenGpioExample.java  
- * 
- * This file is part of the Pi4J project. More information about 
+ * FILENAME      :  ListenGpioExample.java
+ *
+ * This file is part of the Pi4J project. More information about
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
@@ -18,12 +18,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -40,15 +40,15 @@ import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 /**
  * This example code demonstrates how to setup a listener
- * for GPIO pin state changes on the Raspberry Pi.  
- * 
+ * for GPIO pin state changes on the Raspberry Pi.
+ *
  * @author Robert Savage
  */
 public class ListenGpioExample {
-    
+
     public static void main(String args[]) throws InterruptedException {
         System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
-        
+
         // create gpio controller
         final GpioController gpio = GpioFactory.getInstance();
 
@@ -62,19 +62,19 @@ public class ListenGpioExample {
                 // display pin state on console
                 System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
             }
-            
+
         });
-        
+
         System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
-        
+
         // keep program running until user aborts (CTRL-C)
         while(true) {
             Thread.sleep(500);
         }
-        
+
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller        
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
     }
 }
 
