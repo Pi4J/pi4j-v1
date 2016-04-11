3a9db411b9f4f9249c0f1996b8ec0d634632462d
Robert Savage <robert@pi4j.com>
2016-04-11 11:40:52 -0400

    fixed #206; implemented try-with-resources and process destroy for ExecUtil.execute().

--- pi4j-core/src/main/java/com/pi4j/util/ExecUtil.java
+++ pi4j-core/src/main/java/com/pi4j/util/ExecUtil.java
@@ -44,36 +44,47 @@ public class ExecUtil
 
     public static String[] execute(String command, String split) throws IOException, InterruptedException {
         List<String> result = new ArrayList<>();
+
+        // create external process
         Process p = Runtime.getRuntime().exec(command);
+
+        // wait for external process to complete
         p.waitFor();
 
-        if(p.exitValue() != 0)
+        // if the external proess returns an error code (non-zero), then build out and return null
+        if(p.exitValue() != 0) {
+            p.destroy();
             return null;
+        }
 
-        InputStreamReader isr = new InputStreamReader(p.getInputStream());
-        BufferedReader reader = new BufferedReader(isr);
-        String line = reader.readLine();
-        while (line != null) {
-            if (!line.isEmpty()) {
-                if (split == null || split.isEmpty()) {
-                    result.add(line.trim());
-                } else {
-                    String[] parts = line.trim().split(split);
-                    for(String part : parts) {
-                        if (part != null && !part.isEmpty()) {
-                            result.add(part.trim());
+        // using try-with-resources to ensure closure
+        try(InputStreamReader isr = new InputStreamReader(p.getInputStream());
+            BufferedReader reader = new BufferedReader(isr)) {
+            // read lines from buffered reader
+            String line = reader.readLine();
+            while (line != null) {
+                if (!line.isEmpty()) {
+                    if (split == null || split.isEmpty()) {
+                        result.add(line.trim());
+                    } else {
+                        String[] parts = line.trim().split(split);
+                        for(String part : parts) {
+                            if (part != null && !part.isEmpty()) {
+                                result.add(part.trim());
+                            }
                         }
                     }
                 }
+
+                // read next line
+                line = reader.readLine();
             }
-            line = reader.readLine();
         }
 
-        // close readers and stream
-        reader.close();
-        isr.close();
-        p.getInputStream().close();
+        // destroy process
+        p.destroy();
 
+        // return result
         if (result.size() > 0) {
             return result.toArray(new String[result.size()]);
         } else {
