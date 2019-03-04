ceb37abb17b5c74c1ae1513c6be6e07a775af08a
Robert Savage <robert@shadeblue.com>
2019-03-04 14:53:11 -0500

    updated source headers for new HTTPS site

--- pi4j-core/src/main/java/com/pi4j/io/spi/SpiMode.java
+++ pi4j-core/src/main/java/com/pi4j/io/spi/SpiMode.java
@@ -8,7 +8,7 @@ package com.pi4j.io.spi;
  * FILENAME      :  SpiMode.java
  *
  * This file is part of the Pi4J project. More information about
- * this project can be found here:  https://www.pi4j.com/
+ * this project can be found here:  https://pi4j.com/
  * **********************************************************************
  * %%
  * Copyright (C) 2012 - 2019 Pi4J
