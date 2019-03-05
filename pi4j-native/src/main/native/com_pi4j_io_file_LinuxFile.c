bc9c46782239bfd817d8f003d97ddbcecaceaa4b
Robert Savage <robert@shadeblue.com>
2019-03-04 21:23:20 -0500

    updated C++ comment headers

--- pi4j-native/src/main/native/com_pi4j_io_file_LinuxFile.c
+++ pi4j-native/src/main/native/com_pi4j_io_file_LinuxFile.c
@@ -4,7 +4,7 @@
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: JNI Native Library
  * FILENAME      :  com_pi4j_io_file_LinuxFile.c
- *
+ * 
  * This file is part of the Pi4J project. More information about
  * this project can be found here:  https://pi4j.com/
  * **********************************************************************
@@ -15,12 +15,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
