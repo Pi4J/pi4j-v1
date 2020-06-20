3112f67ed95a35d32b52e8cffd87b1129482b8d9
Robert Savage <robert@pi4j.com>
2020-06-20 14:55:34 -0400

    updated copyrights for 2020

--- pi4j-core/src/main/java/com/pi4j/io/serial/tasks/SerialDataEventDispatchTaskImpl.java
+++ pi4j-core/src/main/java/com/pi4j/io/serial/tasks/SerialDataEventDispatchTaskImpl.java
@@ -11,7 +11,7 @@ package com.pi4j.io.serial.tasks;
  * this project can be found here:  https://pi4j.com/
  * **********************************************************************
  * %%
- * Copyright (C) 2012 - 2019 Pi4J
+ * Copyright (C) 2012 - 2020 Pi4J
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
